edeafc7506725858845aa962759645c70291d4d4
elliotnam0411 <eknam2@illinois.edu>
2018-12-12 14:00:56 -0600

    Activity 1 Activity 2 (java, xml)

--- app/src/main/java/com/example/elliotnam0411/csfinal/MainActivity.java
+++ app/src/main/java/com/example/elliotnam0411/csfinal/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.elliotnam0411.csfinal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,51 +13,137 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ProgressBar;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.os.AsyncTask;
 
+import java.sql.*;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText Name;
     private EditText Password;
-    private TextView Info;
     private Button Login;
-    private int counter = 5;
-
-
-
-
+    private ProgressBar progressBar;
+    private static Connection myConn;
+    public static String netid;
+    String un,pass,db,ip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
         Name = (EditText)findViewById(R.id.etName);
         Password = (EditText)findViewById(R.id.etPassword);
-        Info = (TextView)findViewById(R.id.tvInfo);
         Login = (Button)findViewById(R.id.btnLogin);
-        Info.setText("You have 5 attempts left to Login");
+        progressBar.setVisibility(View.GONE);
+        ip = "61.73.127.241";
+        db = "uiuc_delivery";
+        un = "root";
+        pass = "kenken@14mysql";
+        //myConn = connectionClass(un,pass,db,ip);
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                Validate validate = new Validate();
+                validate.execute();
             }
         });
+    }
+    public class Validate extends AsyncTask<String, String, String> {
+        String z = "";
+        Boolean isSuccess = false;
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+        protected String doInBackground(String...params) {
+
+            String username = Name.getText().toString();
+            String password = Password.getText().toString();
+            if (username.trim().equals("") || password.trim().equals("")) {
+                z = "Please enter Username and Password";
+            } else {
+                try {
+                    myConn = connectionClass(un,pass,db,ip);
+                    if (myConn == null) {
+                        z = "Check your Internet Connection";
+                    } else {
+                        String query = " ";
+                        Statement stmt = myConn.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+                        if (rs.next()) { //while(rs.next) { }
+                            netid = username;
+                            z = "Login Successful";
+                            isSuccess = true;
+                            myConn.close();
+                            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                            startActivity(intent);
+                        } else {
+                            z = "Invalid Credentials";
+                            isSuccess = false;
+                        }
+                    }
+                } catch (Exception e) {
+                    isSuccess = false;
+                    z = e.getMessage();
+                }
+            }
+            return z;
 
         }
-
-    private void validate(String userName, String userPassword) {
-        if ((userName.equals("aaa")) && (userPassword.equals("1234"))) {
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
-        } else {
-            counter--;
-            Info.setText("You have " + String.valueOf(counter) + "attempts left to Login");
-            if (counter == 0) {
-                Login.setEnabled(false); //cancelling button
+        protected void onPostExecute(String r) {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, r, Toast.LENGTH_LONG).show();
+            if (isSuccess) {
+                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
             }
         }
     }
+    /*
+    private void validate(String userName, String userPassword) {
+        int resultSize = 0;
+            String query = "SELECT user_password FROM user_table WHERE user_netid = 'a' AND user_password = 'a'";
+            try {
+                myConn = DriverManager.getConnection(url, "root", "kenken@14mysql");
+                myStmt = myConn.createStatement();
+                myRs = myStmt.executeQuery(query);
+                while (myRs.next()) {
+                    resultSize++;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            if (resultSize==1) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            } else {
+                Login.setEnabled(false); //cancelling button
+            }
+        }
+        */
+
+    /*finally {
+            try {
+                myConn.close();
+            } catch (SQLException e) {}
+            try {
+                myStmt.close();
+            } catch (SQLException e) {}
+            try {
+                myRs.close();
+            } catch (SQLException e) {}
+        }*/
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -79,4 +166,28 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public Connection connectionClass(String user, String password, String database, String server) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try {
+            //Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
+            ConnectionURL = "jdbc:mysql://" + server + ":3306/" + database;
+            connection = DriverManager.getConnection(ConnectionURL,user,password);
+        }
+        catch (ClassNotFoundException e) {
+            Log.e("error here 1 :", e.getMessage());
+        }
+        catch (SQLException e) {
+            Log.e("error 2: ", e.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("error 3: ", e.getMessage());
+        }
+        return connection;
+    }
 }
+
+
--- app/src/main/java/com/example/elliotnam0411/csfinal/SecondActivity.java
+++ app/src/main/java/com/example/elliotnam0411/csfinal/SecondActivity.java
@@ -3,13 +3,212 @@ package com.example.elliotnam0411.csfinal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.os.AsyncTask;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import android.os.StrictMode;
 import org.w3c.dom.Text;
+//package com.example.rzleb.adminloginpage;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.Toast;
+import android.widget.ProgressBar;
+
+public class SecondActivity extends AppCompatActivity {
+    static ResultSet rs;
+    static PreparedStatement st;
+    static Connection con;
+    String str = "new";
+    String un,pass,db,ip;
+    Statement stmt = null;
+    final int dorm_id = 1;
+    Button button1, button2, button3, button4, button5;
+    TextView date1,date2,date3,date4,date5;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        final Button button1 = (Button) findViewById(R.id.firstbutton);
+        button1.setEnabled(false);
+        final Button button2 = (Button) findViewById(R.id.secondbutton);
+        button2.setEnabled(false);
+        final Button button3 = (Button) findViewById(R.id.thirdbutton);
+        button3.setEnabled(false);
+        final Button button4 = (Button) findViewById(R.id.fourthbutton);
+        button4.setEnabled(false);
+        final Button button5 = (Button) findViewById(R.id.fifthbutton);
+        button5.setEnabled(false);
+        final TextView date1 = (TextView) findViewById(R.id.firstdate);
+        final TextView date2 = (TextView) findViewById(R.id.seconddate);
+        final TextView date3 = (TextView) findViewById(R.id.thirddate);
+        final TextView date4 = (TextView) findViewById(R.id.fourthdate);
+        final TextView date5 = (TextView) findViewById(R.id.fifthdate);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        ip = "61.73.127.241";
+        db = "uiuc_package";
+        un = "root";
+        pass = "kenken@14mysql";
+        Connection connection = null;
+        String ConnectionURL = null;
+
+        RetrievePackage retrievePackage = new RetrievePackage();
+        retrievePackage.execute();
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button1.setEnabled(false);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button2.setEnabled(false);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button3.setEnabled(false);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button4.setEnabled(false);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button5.setEnabled(false);
+            }
+        });
+
+
+    }
+    public class RetrievePackage extends AsyncTask<String, String, String> {
+        String z = "";
+        Boolean isSuccess = false;
+        String ConnectionURL;
+        Connection connection;
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+        protected String doInBackground(String...params) {
+            ArrayList result = new ArrayList();
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+                //jdbc:mysql://127.0.0.1:3306/expeditor?zeroDateTimeBehavior=convertToNull&user=root&password=onelife
+                ConnectionURL = "jdbc:mysql://" + ip +":3306/" + db +"?zeroDateTimeBehavior=convertToNull";
+                connection = DriverManager.getConnection(ConnectionURL,un,pass);
+
+                Class.forName("com.mysql.jdbc.Driver");
+                Class.forName("net.sourceforge.jtds.jdbc.Driver");
+                ConnectionURL = "jdbc:mysql://" + ip + "/" + db +"?useUnicode=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+                connection = DriverManager.getConnection(ConnectionURL,un,pass);
+
+
+                //  이곳이 배달온 package list 넘기는 부분.
+                st=connection.prepareStatement("select package_no,package_delivery_date from package_table where package_user_netid = " + MainActivity.netid + " and package_delivery_status = 0");
+                rs=st.executeQuery();
+
+                int count = 0;
+                while (count < 5 && rs.next()) {
+                    if (count == 0) {
+                        date1.setText(rs.getTimestamp("package_delivery_date").toString());
+                        button1.setEnabled(true);
+                    }
+                    if (count == 1 && rs.next()) {
+                        date2.setText(rs.getTimestamp("package_delivery_date").toString());
+                        button2.setEnabled(true);
+                    }
+                    if (count == 2 && rs.next()) {
+                        date3.setText(rs.getTimestamp("package_delivery_date").toString());
+                        button3.setEnabled(true);
+                    }
+                    if (count == 3 && rs.next()) {
+                        date4.setText(rs.getTimestamp("package_delivery_date").toString());
+                        button4.setEnabled(true);
+                    }
+                    if (count == 4 && rs.next()) {
+                        date5.setText(rs.getTimestamp("package_delivery_date").toString());
+                        button5.setEnabled(true);
+                    }
+                    count++;
+                }
+
+
+                /*** test part
+                 java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd/MM/yyyy");
+                 java.util.Date d = sdf.parse("11/12/2018");
+                 Package tt = new Package(1,d);
+                 result.add(tt);
+                 ****/
+
+
+
+                ////******************  여기까지
+            }
+            catch(Exception e) {
+                    e.printStackTrace();
+                //tv.setText(str);
+            }
+            z = "";
+            return z;
+        }
+        protected void onPostExecute(String r) {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(SecondActivity.this, r, Toast.LENGTH_LONG).show();
+
+        }
+    }
+    public Connection connectionClass(String user, String password, String database, String server) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try {
+            //Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
+            ConnectionURL = "jdbc:mysql://" + server + ":3306/" + database;
+            connection = DriverManager.getConnection(ConnectionURL,user,password);
+        }
+        catch (ClassNotFoundException e) {
+            Log.e("error here 1 :", e.getMessage());
+        }
+        catch (SQLException e) {
+            Log.e("error 2: ", e.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("error 3: ", e.getMessage());
+        }
+        return connection;
+    }
+}
+
+
+/*
 public class SecondActivity extends AppCompatActivity {
 
     private EditText RoomNumber;
@@ -25,9 +224,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        name1.setText("aa");
-        Intent intent = new Intent;
-        /*RoomNumber = (EditText)findViewById(R.id.roomNumber);
+        RoomNumber = (EditText)findViewById(R.id.roomNumber);
         name1 = (TextView)findViewById(R.id.n1);
         name2 = (TextView)findViewById(R.id.n2);
         name3 = (TextView)findViewById(R.id.n3);
@@ -35,11 +232,9 @@ public class SecondActivity extends AppCompatActivity {
         ResidentName = (EditText)findViewById(R.id.firstName);
         message = (TextView)findViewById(R.id.messageSent);
         residentQuery(RoomNumber.getText().toString());
-        sendMessage(ResidentName.getText().toString());*/
+        sendMessage(ResidentName.getText().toString());
     }
     protected void residentQuery(String roomNumber) {
-        Intent intent = new Intent(SecondActivity.this, );
-        startActivity(intent);
         int roomNum = Integer.parseInt(roomNumber);
         if (roomNum == 713) {
             name1.setText("Hideki Maeda");
@@ -54,3 +249,4 @@ public class SecondActivity extends AppCompatActivity {
         }
     }
 }
+*/
